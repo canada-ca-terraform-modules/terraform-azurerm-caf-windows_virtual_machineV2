098a54c87dea93ff2970fbca4f37544add4c080c
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2024-12-18 08:47:31 -0500

    Added the P3 URL for custom data

--- http.tf
+++ http.tf
@@ -1,5 +1,5 @@
 locals {
-  custom_data_url = strcontains(var.env, "G3") ? "https://g3scexample01ba8b7d81.blob.core.windows.net/testest/testtest.ps1" : "https://gcpcenteslzpublicblob4df.blob.core.windows.net/publicresources/windows-all-customdata-default.ps1"
+  custom_data_url = strcontains(var.env, "G3") ? "https://g3pceslzresentdfa0353e.blob.core.windows.net/publicresources/windows-all-customdata-default.ps1" : "https://gcpcenteslzpublicblob4df.blob.core.windows.net/publicresources/windows-all-customdata-default.ps1"
 }
 
 data "http" "custom_data" {
